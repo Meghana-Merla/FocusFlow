b6315cacf7a8e133c797bbbcc5de1f6da44ba63a
Meghana-Merla <24je0654@iitism.ac.in>
2026-05-16 23:43:22 +0530

    Implement countdown timer functionality

--- app/src/main/java/com/meghana/focusflow/MainActivity.kt
+++ app/src/main/java/com/meghana/focusflow/MainActivity.kt
@@ -1,20 +1,70 @@
 package com.meghana.focusflow
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.CountDownTimer
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvTimer: TextView
+    private lateinit var btnStart: Button
+
+    private var timeLeftInMillis: Long = 1500000
+    private var timerRunning = false
+
+    private lateinit var countDownTimer: CountDownTimer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        tvTimer = findViewById(R.id.tvTimer)
+        btnStart = findViewById(R.id.btnStart)
+
+        updateTimerText()
+
+        btnStart.setOnClickListener {
+
+            if (!timerRunning) {
+                startTimer()
+            }
         }
+    }
+
+    private fun startTimer() {
+
+        countDownTimer = object : CountDownTimer(timeLeftInMillis, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+
+                timeLeftInMillis = millisUntilFinished
+                updateTimerText()
+            }
+
+            override fun onFinish() {
+
+                timerRunning = false
+            }
+        }.start()
+
+        timerRunning = true
+    }
+
+    private fun updateTimerText() {
+
+        val minutes = (timeLeftInMillis / 1000) / 60
+        val seconds = (timeLeftInMillis / 1000) % 60
+
+        val timeFormatted = String.format(
+            Locale.getDefault(),
+            "%02d:%02d",
+            minutes,
+            seconds
+        )
+
+        tvTimer.text = timeFormatted
     }
 }
